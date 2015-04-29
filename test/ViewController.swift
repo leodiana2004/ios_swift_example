224461949cdb3e4c3b5ad16bfe92702668f00472
Leo <Leo@LeodeMacBook-Pro.local>
2015-04-29 23:03:34 +0800

    programing UILable

---
+++ test/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Leo on 2015/4/29.
+//  Copyright (c) 2015年 Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var shoeUIButton: UIButton!
+    @IBAction func showAction(sender: AnyObject) {
+        
+        showUILabel.text = "Hello World!"
+        //self.performSegueWithIdentifier("changeTo", sender: self)
+        
+        var label = UILabel(frame: CGRectMake(0, 0, 200, 21))
+        label.center = CGPointMake(160, 284)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "Hello World! 2"
+        self.view.addSubview(label)
+        
+    }
+    @IBOutlet weak var showUILabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
